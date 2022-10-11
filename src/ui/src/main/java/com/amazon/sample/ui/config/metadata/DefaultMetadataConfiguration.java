90deb256dc631f918b32daa6df695adcef20ca01
Niall Thomson <thomson.niall@gmail.com>
2022-10-11 08:56:32 -0600

    Remove EC2 metadata discovery for now

--- src/ui/src/main/java/com/amazon/sample/ui/config/metadata/DefaultMetadataConfiguration.java
+++ src/ui/src/main/java/com/amazon/sample/ui/config/metadata/DefaultMetadataConfiguration.java
@@ -1,16 +1,11 @@
 package com.amazon.sample.ui.config.metadata;
 
 import com.amazon.sample.ui.services.Metadata;
-import com.amazon.sample.ui.util.ecs.AwsECSEnvironmentCondition;
-import com.amazon.sample.ui.util.ecs.MissingAwsECSEnvironmentCondition;
-import io.awspring.cloud.context.annotation.ConditionalOnMissingAwsCloudEnvironment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnMissingAwsCloudEnvironment
-@Conditional(MissingAwsECSEnvironmentCondition.class)
 public class DefaultMetadataConfiguration {
 
     @Bean
