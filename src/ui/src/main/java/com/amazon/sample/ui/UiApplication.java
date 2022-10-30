6e9564234fee46eb1786ee2b61d766bbd67fa6ef
Niall Thomson <thomson.niall@gmail.com>
2022-10-30 16:00:31 +0000

    Removed unused member field from UI app class

--- src/ui/src/main/java/com/amazon/sample/ui/UiApplication.java
+++ src/ui/src/main/java/com/amazon/sample/ui/UiApplication.java
@@ -18,14 +18,11 @@
 
 package com.amazon.sample.ui;
 
-import com.amazon.sample.ui.clients.catalog.ApiClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UiApplication {
-
-	private ApiClient client;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
