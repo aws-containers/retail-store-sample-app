0a0ae36f159f5f3d4b1c022493d5733336e45dae
Niall Thomson <thomson.niall@gmail.com>
2022-12-02 21:34:18 +0000

    Make it possible to run the UI component with selective mock services

--- src/ui/src/main/java/com/amazon/sample/ui/config/StoreServices.java
+++ src/ui/src/main/java/com/amazon/sample/ui/config/StoreServices.java
@@ -22,43 +22,74 @@ import com.amazon.sample.ui.clients.carts.api.CartsApi;
 import com.amazon.sample.ui.clients.carts.api.ItemsApi;
 import com.amazon.sample.ui.clients.catalog.api.CatalogApi;
 import com.amazon.sample.ui.clients.checkout.api.CheckoutApi;
-import com.amazon.sample.ui.clients.orders.api.OrdersApi;
+import com.amazon.sample.ui.services.assets.AssetsService;
+import com.amazon.sample.ui.services.assets.MockAssetsService;
+import com.amazon.sample.ui.services.assets.ProxyingAssetsService;
 import com.amazon.sample.ui.services.carts.CartsService;
+import com.amazon.sample.ui.services.carts.MockCartsService;
 import com.amazon.sample.ui.services.carts.WebClientCartsService;
 import com.amazon.sample.ui.services.catalog.CatalogService;
+import com.amazon.sample.ui.services.catalog.MockCatalogService;
 import com.amazon.sample.ui.services.catalog.WebClientCatalogService;
 import com.amazon.sample.ui.services.catalog.model.CatalogMapper;
 import com.amazon.sample.ui.services.checkout.CheckoutService;
+import com.amazon.sample.ui.services.checkout.MockCheckoutService;
 import com.amazon.sample.ui.services.checkout.WebClientCheckoutService;
 import com.amazon.sample.ui.services.checkout.model.CheckoutMapper;
-import com.amazon.sample.ui.services.orders.OrdersService;
-import com.amazon.sample.ui.services.orders.WebClientOrdersService;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!stubs")
-public class WebClientServices {
+public class StoreServices {
 
     @Bean
+    @ConditionalOnProperty(prefix = "endpoints", name = "catalog")
     public CatalogService catalogService(CatalogApi catalogApi, CatalogMapper mapper) {
         return new WebClientCatalogService(catalogApi, mapper);
     }
 
     @Bean
+    @ConditionalOnProperty(prefix = "endpoints", name = "catalog", havingValue = "false", matchIfMissing = true)
+    public CatalogService mockCatalogService() {
+        return new MockCatalogService();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "endpoints", name = "carts")
     public CartsService cartsService(CartsApi cartsApi, ItemsApi itemsApi, CatalogService catalogService) {
         return new WebClientCartsService(cartsApi, itemsApi, catalogService);
     }
 
+
     @Bean
+    @ConditionalOnProperty(prefix = "endpoints", name = "carts", havingValue = "false", matchIfMissing = true)
+    public CartsService mockCartsService(CatalogService catalogService) {
+        return new MockCartsService(catalogService);
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "endpoints", name = "checkout")
     public CheckoutService checkoutService(CheckoutApi api, CheckoutMapper mapper, CartsService cartsService) {
         return new WebClientCheckoutService(api, mapper, cartsService);
     }
 
     @Bean
-    public OrdersService ordersService(OrdersApi ordersApi, CartsService cartsService) {
-        return new WebClientOrdersService(ordersApi, cartsService);
+    @ConditionalOnProperty(prefix = "endpoints", name = "checkout", havingValue = "false", matchIfMissing = true)
+    public CheckoutService mockCheckoutService(CheckoutMapper mapper, CartsService cartsService) {
+        return new MockCheckoutService(mapper, cartsService);
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "endpoints", name = "assets")
+    public AssetsService<?> assetsService() {
+        return new ProxyingAssetsService();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "endpoints", name = "assets", havingValue = "false", matchIfMissing = true)
+    public AssetsService<?> mockAssetsService() {
+        return new MockAssetsService();
     }
 }
--- src/ui/src/main/java/com/amazon/sample/ui/services/assets/MockAssetsService.java
+++ src/ui/src/main/java/com/amazon/sample/ui/services/assets/MockAssetsService.java
@@ -16,39 +16,24 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package com.amazon.sample.ui.web;
+package com.amazon.sample.ui.services.assets;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.CacheControl;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import com.amazon.sample.ui.services.assets.AssetsService;
-
 import reactor.core.publisher.Mono;
 
 import java.util.concurrent.TimeUnit;
 
-/**
- * This controller serves product images from the catalog backend, adding cache control headers along the way
- */
-@RestController
-public class CatalogImageController {
+public class MockAssetsService implements AssetsService<ClassPathResource> {
 
-  private AssetsService assetsService;
-
-  public CatalogImageController(@Autowired AssetsService assetsService) {
-      this.assetsService = assetsService;
-  }
-
-    @GetMapping(value = "/assets/{image}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public Mono<ResponseEntity<byte[]>> catalogueImage(@PathVariable String image) {
-        return this.assetsService.getImage(image);
+    public Mono<ResponseEntity<ClassPathResource>> getImage(String image) {
+      ClassPathResource classPathResource = new ClassPathResource("static/assets/img/sample_product.jpg");
+      
+      return Mono.just(new ResponseEntity<ClassPathResource>(classPathResource, HttpStatus.OK));
     }
 }
