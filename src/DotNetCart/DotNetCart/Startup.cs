dc5ea36a2b51def507ec3ee07f049955b9477b89
Steven David <stevendavid@outlook.com>
2023-07-10 22:59:25 -0700

    added the OpenTelemetry reference and implementation

--- src/DotNetCart/DotNetCart/Startup.cs
+++ src/DotNetCart/DotNetCart/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using OpenTelemetry.Trace;
 
 namespace DotNetCart
 {
@@ -31,6 +25,13 @@ namespace DotNetCart
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DotNetCart", Version = "v1" });
             });
+            services.AddOpenTelemetry()
+                .WithTracing(b =>
+                {
+                    b
+                        .AddHttpClientInstrumentation()
+                        .AddAspNetCoreInstrumentation();
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
