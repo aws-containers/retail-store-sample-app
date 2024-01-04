4597b8962bc1717a6453e17dd041796c3d46941b
Niall Thomson <thomson.niall@gmail.com>
2024-01-04 00:36:18 +0000

    Migrate RabbitMQ broker to base TF provider

--- deploy/terraform/lib/dependencies/mq.tf
+++ deploy/terraform/lib/dependencies/mq.tf
@@ -1,29 +1,54 @@
 locals {
-  mq_default_user = "default_mq_user"
-}
-
-module "orders_mq_broker" {
-  source  = "cloudposse/mq-broker/aws"
-  version = "3.0.0"
-
-  name                       = "${var.environment_name}_orders_mq-broker"
-  vpc_id                     = var.vpc_id
-  subnet_ids                 = [var.subnet_ids[0]]
-  mq_admin_user              = [local.mq_default_user]
-  mq_admin_password          = [random_password.mq_password.result]
-  deployment_mode            = "SINGLE_INSTANCE"
-  engine_type                = "RabbitMQ"
-  engine_version             = "3.10.10"
-  audit_log_enabled          = false
-  encryption_enabled         = false
-
+  mq_default_user            = "default_mq_user"
   allowed_security_group_ids = concat(var.allowed_security_group_ids, [var.orders_security_group_id])
-
-  tags             = var.tags
 }
 
 resource "random_password" "mq_password" {
   length           = 16
   special          = true
   override_special = "!#$%&*()-_+{}<>?"
+}
+
+resource "aws_mq_broker" "mq" {
+  broker_name = "${var.environment_name}-orders-broker"
+
+  engine_type         = "RabbitMQ"
+  engine_version      = "3.10.10"
+  host_instance_type  = "mq.t3.micro"
+  deployment_mode     = "SINGLE_INSTANCE"
+  subnet_ids          = [var.subnet_ids[0]]
+  security_groups     = [aws_security_group.mq.id]
+  apply_immediately   = true
+  publicly_accessible = false
+
+  user {
+    username = local.mq_default_user
+    password = random_password.mq_password.result
+  }
+
+  tags = var.tags
+}
+
+resource "aws_security_group" "mq" {
+  name        = "${var.environment_name}-orders-broker"
+  vpc_id      = var.vpc_id
+  description = "Secure traffic to Rabbit MQ"
+
+  tags = merge(var.tags, { Name = "${var.environment_name}-orders-broker" })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "mq" {
+  for_each = toset(local.allowed_security_group_ids)
+
+  type              = "ingress"
+  from_port         = 5671
+  to_port           = 5671
+  protocol          = "tcp"
+  security_group_id = aws_security_group.mq.id
+
+  source_security_group_id = each.key
 }
