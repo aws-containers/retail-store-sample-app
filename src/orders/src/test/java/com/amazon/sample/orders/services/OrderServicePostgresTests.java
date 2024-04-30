a860b719303ffa920115a71c70c57da99a911394
Niall Thomson <thomson.niall@gmail.com>
2024-04-30 16:41:34 -0600

    Enable default profile in tests

--- src/orders/src/test/java/com/amazon/sample/orders/services/OrderServicePostgresTests.java
+++ src/orders/src/test/java/com/amazon/sample/orders/services/OrderServicePostgresTests.java
@@ -30,6 +30,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -41,6 +42,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("default")
 public class OrderServicePostgresTests {
 
   @LocalServerPort
