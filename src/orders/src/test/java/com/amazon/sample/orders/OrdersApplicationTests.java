8dfd438751399c72b1f2fde98cb7085772e6a9f4
Niall Thomson <thomson.niall@gmail.com>
2024-05-07 13:48:55 -0600

    Tidied Java

--- src/orders/src/test/java/com/amazon/sample/orders/OrdersApplicationTests.java
+++ src/orders/src/test/java/com/amazon/sample/orders/OrdersApplicationTests.java
@@ -20,7 +20,6 @@ package com.amazon.sample.orders;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class OrdersApplicationTests {
--- src/orders/src/test/java/com/amazon/sample/orders/services/OrderServicePostgresTests.java
+++ src/orders/src/test/java/com/amazon/sample/orders/services/OrderServicePostgresTests.java
@@ -30,7 +30,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
