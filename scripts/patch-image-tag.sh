1e0d3e72632f0fc6bddfd6e3a91e8de83928d00e
Niall Thomson <thomson.niall@gmail.com>
2024-07-12 17:17:41 -0600

    Patch helm chart versions with image tags

--- scripts/patch-image-tag.sh
+++ scripts/patch-image-tag.sh
@@ -11,9 +11,20 @@ patch_values() {
   diff <(echo "$orig") <(echo "$changed") | patch $1
 }
 
+patch_chart() {
+  orig=$(yq eval --exit-status '.' $1)
+  changed=$(yq eval --exit-status ".version = \"$TAG\"" $1)
+
+  diff_changed=$()
+
+  diff <(echo "$orig") <(echo "$changed") | patch $1
+}
+
 export -f patch_values
+export -f patch_chart
 
 find deploy/kubernetes/charts/ -name "values.yaml" -not -path "*/opentelemetry/*" -exec bash -c 'patch_values ${0}' '{}' \;
+find deploy/kubernetes/charts/ -name "Chart.yaml" -not -path "*/opentelemetry/*" -exec bash -c 'patch_chart ${0}' '{}' \;
 # Clean up dirty patch files
 find deploy/kubernetes/charts '('     -name \*-baseline -o     -name \*-merge -o     -name \*-original -o     -name \*.orig -o     -name \*.rej ')' -delete -type f
 
@@ -33,3 +44,4 @@ yq -i ".services.assets.image = \"public.ecr.aws/aws-containers/retail-store-sam
 
 # Template Terraform image default
 cat deploy/terraform/lib/images/generated.tf.json | jq -Mr ". | .locals.published_tag = \"$TAG\"" | tee deploy/terraform/lib/images/generated.tf.json
+
