a6833957bbd4c6e0211e835ac1007b0d94b54dd6
Niall Thomson <thomson.niall@gmail.com>
2024-12-16 10:00:34 -0700

    Patch image tag to app chart dependencies

--- scripts/patch-image-tag.sh
+++ scripts/patch-image-tag.sh
@@ -23,25 +23,31 @@ patch_chart() {
 export -f patch_values
 export -f patch_chart
 
-find deploy/kubernetes/charts/ -name "values.yaml" -not -path "*/opentelemetry/*" -exec bash -c 'patch_values ${0}' '{}' \;
+find deploy/kubernetes/charts/ -name "values.yaml" -not -path "*/opentelemetry/*" -not -path "*/app/*" -exec bash -c 'patch_values ${0}' '{}' \;
 find deploy/kubernetes/charts/ -name "Chart.yaml" -not -path "*/opentelemetry/*" -exec bash -c 'patch_chart ${0}' '{}' \;
+
+outfile=$(mktemp)
+
+yq eval ".dependencies[].version |= \"$TAG\"" deploy/kubernetes/charts/app/Chart.yaml > $outfile
+cp $outfile deploy/kubernetes/charts/app/Chart.yaml
+
 # Clean up dirty patch files
 find deploy/kubernetes/charts '('     -name \*-baseline -o     -name \*-merge -o     -name \*-original -o     -name \*.orig -o     -name \*.rej ')' -delete -type f
-
-mkdir -p dist/kubernetes dist/docker-compose
-
-# Template Kubernetes YAML
-LOAD_BALANCER=yes helmfile -f deploy/kubernetes/charts/helmfile.yaml template --skip-tests > dist/kubernetes/deploy.yaml
-
-# Template docker-compose
-cp deploy/docker-compose/docker-compose.yml dist/docker-compose/docker-compose.yml
-yq -i ".services.ui.image = \"public.ecr.aws/aws-containers/retail-store-sample-ui:$TAG\"" dist/docker-compose/docker-compose.yml
-yq -i ".services.catalog.image = \"public.ecr.aws/aws-containers/retail-store-sample-catalog:$TAG\"" dist/docker-compose/docker-compose.yml
-yq -i ".services.carts.image = \"public.ecr.aws/aws-containers/retail-store-sample-cart:$TAG\"" dist/docker-compose/docker-compose.yml
-yq -i ".services.checkout.image = \"public.ecr.aws/aws-containers/retail-store-sample-checkout:$TAG\"" dist/docker-compose/docker-compose.yml
-yq -i ".services.orders.image = \"public.ecr.aws/aws-containers/retail-store-sample-orders:$TAG\"" dist/docker-compose/docker-compose.yml
-yq -i ".services.assets.image = \"public.ecr.aws/aws-containers/retail-store-sample-assets:$TAG\"" dist/docker-compose/docker-compose.yml
-
-# Template Terraform image default
-cat deploy/terraform/lib/images/generated.tf.json | jq -Mr ". | .locals.published_tag = \"$TAG\"" | tee deploy/terraform/lib/images/generated.tf.json
+#
+#mkdir -p dist/kubernetes dist/docker-compose
+#
+## Template Kubernetes YAML
+#LOAD_BALANCER=yes helmfile -f deploy/kubernetes/charts/helmfile.yaml template --skip-tests > dist/kubernetes/deploy.yaml
+#
+## Template docker-compose
+#cp deploy/docker-compose/docker-compose.yml dist/docker-compose/docker-compose.yml
+#yq -i ".services.ui.image = \"public.ecr.aws/aws-containers/retail-store-sample-ui:$TAG\"" dist/docker-compose/docker-compose.yml
+#yq -i ".services.catalog.image = \"public.ecr.aws/aws-containers/retail-store-sample-catalog:$TAG\"" dist/docker-compose/docker-compose.yml
+#yq -i ".services.carts.image = \"public.ecr.aws/aws-containers/retail-store-sample-cart:$TAG\"" dist/docker-compose/docker-compose.yml
+#yq -i ".services.checkout.image = \"public.ecr.aws/aws-containers/retail-store-sample-checkout:$TAG\"" dist/docker-compose/docker-compose.yml
+#yq -i ".services.orders.image = \"public.ecr.aws/aws-containers/retail-store-sample-orders:$TAG\"" dist/docker-compose/docker-compose.yml
+#yq -i ".services.assets.image = \"public.ecr.aws/aws-containers/retail-store-sample-assets:$TAG\"" dist/docker-compose/docker-compose.yml
+#
+## Template Terraform image default
+#cat deploy/terraform/lib/images/generated.tf.json | jq -Mr ". | .locals.published_tag = \"$TAG\"" | tee deploy/terraform/lib/images/generated.tf.json
 
