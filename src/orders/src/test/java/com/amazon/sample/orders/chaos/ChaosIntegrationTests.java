68bd93b7733f4fc0b8f4b6ecb46ae981b2ca53e6
Niall Thomson <thomson.niall@gmail.com>
2025-03-22 05:52:44 +0000

    Fix test

--- src/orders/src/test/java/com/amazon/sample/orders/chaos/ChaosIntegrationTests.java
+++ src/orders/src/test/java/com/amazon/sample/orders/chaos/ChaosIntegrationTests.java
@@ -28,19 +28,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/orders")
-class TestController {
-
-  @GetMapping
-  public ResponseEntity<String> test() {
-    return ResponseEntity.ok("test response");
-  }
-}
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ChaosServiceIntegrationTest {
