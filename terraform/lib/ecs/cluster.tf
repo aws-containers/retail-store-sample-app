7e081fb79f73faec356f155454461f6eaa54fd04
jakesky <jakesky@amazon.co.uk>
2025-04-01 10:47:11 +0100

    format

--- terraform/lib/ecs/cluster.tf
+++ terraform/lib/ecs/cluster.tf
@@ -8,7 +8,7 @@ resource "aws_ecs_cluster" "cluster" {
 }
 
 resource "aws_cloudwatch_log_group" "ecs_tasks" {
-  name              = "${var.environment_name}-tasks"
+  name = "${var.environment_name}-tasks"
 }
 
 resource "aws_service_discovery_private_dns_namespace" "this" {
