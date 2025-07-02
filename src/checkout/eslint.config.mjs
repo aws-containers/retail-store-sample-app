0dd9f6c017f5317f804dc99706e49ff78725fba8
Niall Thomson <thomson.niall@gmail.com>
2025-07-02 12:08:06 +0000

    Fix tests

---
+++ src/checkout/eslint.config.mjs
@@ -0,0 +1,22 @@
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['src/clients/**/*'] },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+);
