f84ef4c704fb1cd4cd4007c91ed0df2cba07d4a5
Niall Thomson <thomson.niall@gmail.com>
2025-07-10 21:38:17 -0600

    fix: Remove catalog in-memory db logging

--- src/catalog/repository/repository.go
+++ src/catalog/repository/repository.go
@@ -41,20 +41,8 @@ func NewRepository(config config.DatabaseConfiguration) (CatalogRepository, erro
 		db, err = createMySQLDatabase(config)
 	} else {
 		fmt.Println("Using in-memory database")
-		newLogger := logger.New(
-			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
-			logger.Config{
-				SlowThreshold:             time.Second, // Slow SQL threshold
-				LogLevel:                  logger.Info, // Log level
-				IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
-				ParameterizedQueries:      false,       // Don't include params in the SQL log
-				Colorful:                  false,       // Disable color
-			},
-		)
-
-		db, err = gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{
-			Logger: newLogger,
-		})
+		
+		db, err = gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
 	}
 
 	if err != nil {
